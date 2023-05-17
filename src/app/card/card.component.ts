8c640c282ff651a15fc4e1410ca846b4d47d8f98
Teles-Gabriel <gabrielteles0609@gmail.com>
2023-05-16 21:36:43 -0300

    teste

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+  produtos:string[]=[]
+  menuType:string = ""
+
+  constructor() {
+    this.produtos = [
+      "mouse",
+      "teclado",
+      "cabo",
+      "fonte"
+    ]
+   }
+
+  ngOnInit(): void {
+  }
+
+  adicionar():void {
+    this.produtos.push("Gabriel")
+  }
+
+  remover(index:number):void {
+    this.produtos.splice(index, 1)
+  }
+}
